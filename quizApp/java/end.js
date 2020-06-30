6ab73a8ef7cd4ee131153e6b255385f266e77175
akanksha-tanu <67199854+akanksha-tanu@users.noreply.github.com>
2020-06-30 17:43:45 +0530

    Update end.js

--- quizApp/java/end.js
+++ quizApp/java/end.js
@@ -5,7 +5,6 @@ const mostRecentScore=localStorage.getItem("mostRecentScore");
 const finalScore=document.getElementById("finalScore");
 finalScore.innerText=mostRecentScore;
 
-//const high=[];
 const highScores=(JSON.parse(localStorage.getItem("highScores")) || []);
 
 const maxHighScore=5;
