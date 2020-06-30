f440e60c22411af2190a2db4bbff0cf35aee09bf
akanksha-tanu <67199854+akanksha-tanu@users.noreply.github.com>
2020-06-30 11:26:18 +0530

    Update end.js

--- quizApp/java/end.js
+++ quizApp/java/end.js
@@ -5,7 +5,8 @@ const mostRecentScore=localStorage.getItem("mostRecentScore");
 const finalScore=document.getElementById("finalScore");
 finalScore.innerText=mostRecentScore;
 
-const highScores=JSON.parse(localStorage.getItem("highScores")) || [];
+//const high=[];
+const highScores=(JSON.parse(localStorage.getItem("highScores")) || []);
 
 const maxHighScore=5;
 
@@ -23,8 +24,8 @@ function saveHighScore(e){
         name:username.value
     };
     highScores.push(score);
-    highScores.sort((a,b) => b.score-a.score) ;
-    highScores.splice(5);
+    //highScores.sort((a,b) => b.score-a.score) ;
+    //highScores.splice(5);
 
     localStorage.setItem("highScores",JSON.stringify(highScores));
     window.location.assign("index.html");
