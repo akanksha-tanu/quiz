cb9ac6125baec9bde36961037d3770fd37cecffb
akanksha-tanu <67199854+akanksha-tanu@users.noreply.github.com>
2020-06-30 11:25:12 +0530

    Update highscore.js

--- quizApp/java/highscore.js
+++ quizApp/java/highscore.js
@@ -1,8 +1,18 @@
 
 var highScoresList=document.getElementById("highScoresList");
+
 const highScores=JSON.parse(localStorage.getItem("highScores")) || [];
+const high=[];
+
+highScores.forEach(function(e){
+    // console.log(e);
+    high.push(e);
+})
+
+high.sort((a,b) => b.score-a.score) ;
+high.splice(5);
 
 
-highScoresList.innerHTML=highScores.map(function(score){
+highScoresList.innerHTML=high.map(function(score){
     return("<li>"+score.name+'  -  '+score.score+"</li>");
 }).join("");
